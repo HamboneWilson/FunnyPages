8e0f95495bbaa45f79258a782d72b686094092e7
HamboneWilson <carr.adam@gmail.com>
2014-08-02 21:10:58 -0400

    Changed CollectionForm to display checkboxselectmultiple widget for the series field.

--- funnypages/viewer/forms.py
+++ funnypages/viewer/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CheckboxSelectMultiple
 from viewer.models import Collection
 
 
 class CollectionForm(ModelForm):
+
     class Meta:
         model = Collection
+        fields = ('name', 'user', 'series')
+        widgets = {
+            'series': CheckboxSelectMultiple,
+        }
