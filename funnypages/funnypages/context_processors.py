d99388edaa54b8bfbdf2f894c4dd58c0061930f7
HamboneWilson <carr.adam@gmail.com>
2014-08-12 16:07:07 -0400

    Changed the anchor tag in ADD A COLLECTION to use a proper Django URL call.

---
+++ funnypages/funnypages/context_processors.py
@@ -0,0 +1,16 @@
+# from viewer.forms import SubmissionForm, CollectionSelectForm, CollectionForm
+#
+#
+# def SubmissionFormContext(request):
+#     form = SubmissionForm
+#     return {'SubForm': form, }
+#
+#
+# def CollectionSelectFormContext(request):
+#     form = CollectionSelectForm
+#     return {'CSForm': form, }
+#
+#
+# def CollectionFormContext(request):
+#     form = CollectionForm
+#     return {'CForm': form, }
